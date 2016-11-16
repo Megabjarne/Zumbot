fe955c81cf23a357ed48753a1c10a0c876c440e4
Thomas Mjåland <megabjarne@hotmail.com>
2016-11-16 12:35:52 +0100

    Implementert ServoTimer2
    Implementerte ServoTimer2

--- Zumbot/CamHandler.cpp
+++ Zumbot/CamHandler.cpp
@@ -1,26 +1,31 @@
-#include <Servo.h>
+
+#include "ServoTimer2.h"
 #include <arduino.h>
 class cameraHandler
 {
   private:
-    const bool DEBUG = true;
-    const int fieldOfView = 90;
+    const bool DEBUG = false;
+    const int fieldOfView = 135;
     const int leftmostAngle = (180 - fieldOfView) >> 1;
     const int dataPoints = fieldOfView + 1;
-    const int sensorThreshold = 150;
+    const int sensorThreshold = 75;
     unsigned char servoPin;
     unsigned char sensorPin;
     int currentAngle = 0;
     int* readings;
     int* calibrationReadings;
-    Servo serv;
+    ServoTimer2 serv;
     bool goingRight = true;
 
   public:
     boolean enemyInView = false;
     int enemyAngle = 0;
+    int enemyWidth = 0;
 
   private:
+    void writeServ(int ang) {
+      serv.write(750 + ((long)1000 * (long)ang) / (long)180);
+    }
     void evaluateData() {
       //glatter ut data, fjerner sensorfeil og liknende
       for (int i = 0; i <= fieldOfView && false ; i++) {
@@ -30,18 +35,11 @@ class cameraHandler
         tempVal += readings[(i + dataPoints + 1) % dataPoints];
         readings[i] = tempVal / 3;                                //setter en avlesning lik gjennomsnittet av avlesningen og avlesningene over og under (avlesningen tatt på +/- 1 grad)
       }
-      //skiller mellom avlesninger over og under en threshold
-      for (int i = 0; i < dataPoints; i++) {
-        readings[i] = (readings[i] > sensorThreshold);          //gjør datapunkt om til 0 hvis ikke over threshold og 1 hvis over threshold
-      }
-      for (int i = 0; i < dataPoints; i++) {
-        Serial.print(readings[i]);
-      } Serial.println();
       //finn lengste sammenhengende rekke med 1'ere (dvs lengste sammenhengende utslag=mest sannsynlig at er et "stort" objekt slik som en zumo)
       int currentStart = 0;
       int bestStart = -1, bestLength = 0;
       for (int i = 0; i < dataPoints; i++) {
-        if (!readings[i]) {
+        if (!(readings[i] > sensorThreshold)) {
           if (i - currentStart > bestLength) {
             bestStart = currentStart;
             bestLength = i - currentStart;
@@ -54,10 +52,11 @@ class cameraHandler
         bestLength = dataPoints - 1 - currentStart;
       }
       //evaluerer datane fra forrige ledd og ser om det kan være en fiende, og hvilken vinkel det i så fall korresponderer til
-      if (bestLength > 4) {
+      if (bestLength > 3) {
         enemyInView = true;
         enemyAngle += leftmostAngle + bestStart + bestLength / 2; //objektets posisjon blir starten av "siluetten" pluss halve lengden, altså midten av siluetten, + hvileposisjonen (data[0] er tatt fra vinkel gitt av leftmostAngle)
-      enemyAngle/=2;
+        enemyAngle /= 2;
+        enemyWidth = bestLength >> 1;
       } else {
         enemyInView = false;                                     //hvis ingen siluetter har stor nok størrelse så er det antakelig ingen fiende i sikte
       }
@@ -65,19 +64,19 @@ class cameraHandler
 
   public:
     void calibrate() {
-      serv.write(leftmostAngle);
+      writeServ(leftmostAngle);
       delay(250);                                           //venter en stund for å være sikker på at servo rekker å nå hvileposisjon
       for (int i = 0; i <= fieldOfView; i++) {
         calibrationReadings[i] = 0;
         for (int j = 0; j < 100; j++) {                       //gjør fem målinger med litt tid imellom
           calibrationReadings[i] += analogRead(sensorPin);  //summerer opp de fem målingene
-          delayMicroseconds(100);
+          delayMicroseconds(25);
         }
         calibrationReadings[i] = calibrationReadings[i] / 100;                       //deler på antallet målinger
-        serv.write(leftmostAngle + i);                      //skriver neste vinkel
-        delay(3);
+        writeServ(leftmostAngle + i);                      //skriver neste vinkel
+        delayMicroseconds(500);
       }
-      serv.write(leftmostAngle);                            //går tilbake til hvileposisjon
+      writeServ(leftmostAngle);                            //går tilbake til hvileposisjon
       delay(250);
     }
 
@@ -91,23 +90,24 @@ class cameraHandler
       servoPin = _servoPin;
       sensorPin = _sensorPin;
       serv.attach(_servoPin);
-      serv.write(90); delay(1000); serv.write(45); delay(1000);
+      writeServ(90);
     }
 
     void tic() {
       readings[currentAngle] = analogRead(sensorPin) - calibrationReadings[currentAngle]; //leser av sensoren og trekker fra kalibreringsverdien
       if (goingRight) {
         currentAngle++;
-      }else{
+      } else {
         currentAngle--;
       }
-      if (currentAngle > fieldOfView || currentAngle<0) {
+      if (currentAngle % 10 == 0) {
         evaluateData();
-        currentAngle=constrain(currentAngle,0,fieldOfView);
-        goingRight=!goingRight;
+      }
+      if (currentAngle > fieldOfView || currentAngle < 0) {
+        currentAngle = constrain(currentAngle, 0, fieldOfView);
+        goingRight = !goingRight;
       } else {
-        serv.write(leftmostAngle + currentAngle);
-
+        writeServ(leftmostAngle + currentAngle);
       }
     }
 
--- Zumbot/Zumbot.ino
+++ Zumbot/Zumbot.ino
@@ -1,47 +1,78 @@
 #include "STL.h"
 #include "FuncQueHandler.cpp"
 #include "CamHandler.cpp"
-#include <ZumoBuzzer.h>
+//#include <ZumoBuzzer.h>
 #include <ZumoMotors.h>
 #include <Pushbutton.h>
 #include <QTRSensors.h>
 //#include <ZumoReflectanceSensorArray.h>
 //#include <Servo.h>
-
-//ZumoMotors motors;
+ZumoMotors motors;
 //ZumoBuzzer buzzer;
 FuncQueHandler factory;
 cameraHandler vision;
+ServoTimer2 serv;
+unsigned long panicTime = 0;
 
 void setup() {
-  //Servo serv;serv.attach(5);serv.write(90);
-  Pushbutton butt(ZUMO_BUTTON);
+  pinMode(4, INPUT);
+  pinMode(5, INPUT);
   Serial.begin(9600);
+  Pushbutton butt(ZUMO_BUTTON);
   butt.waitForButton();
-   vision.attach(5,0);
-  //butt.waitForButton();
+  vision.attach(2, 0);
+  butt.waitForButton();
   Serial.println("calibrate");
   vision.calibrate();
   Serial.println("c2");
-  //butt.waitForButton();
-  pinMode(3,OUTPUT);
-  factory.make_thread(periodic,5,&visionHandler);
-  factory.make_thread(periodic,2000,&debugging);
+  butt.waitForButton();
+  pinMode(3, OUTPUT);
+  factory.make_thread(periodic, 1, &visionHandler);
+  factory.make_thread(periodic, 50, &debugging);
+  factory.make_thread(periodic, 50, &borderCheck);
 }
 
 void loop() {
   factory.tic();
 }
 
-void visionHandler(){
+void visionHandler() {
+  Serial.println("I see!");
   vision.tic();
 }
 
-void debugging(){
-  if (vision.enemyInView){
-    Serial.print("FOUND: ");Serial.println(vision.enemyAngle);
-  }else{
-    Serial.println("Nothing!");
+void debugging() {
+  if (panicTime < millis()) {
+    if (vision.enemyInView ) {
+      //Serial.print("FOUND: "); Serial.println(vision.enemyAngle);
+      if (vision.enemyAngle - vision.enemyWidth > 110) {
+        motors.setSpeeds(200, 100 );
+      } else {
+        if (vision.enemyAngle + vision.enemyWidth < 70) {
+          motors.setSpeeds(100, 200);
+        } else {
+          motors.setSpeeds(400, 400);
+        }
+      }
+    } else {
+      motors.setSpeeds(-100, 100);
+      //Serial.println("Nothing!");
+    }
+  } else {
+    motors.setSpeeds(-400, -400);
   }
 }
 
+void borderCheck() {
+  unsigned char pins [] = {4, 5};
+  QTRSensorsRC qtrs(pins, 2, 500, QTR_NO_EMITTER_PIN);
+  unsigned int sensorArr[2];
+  Serial.print("start");Serial.flush();
+  qtrs.read(sensorArr);
+  Serial.print("end");Serial.flush();
+  if (sensorArr[0] < 500 || sensorArr[1] < 500) {
+    motors.setSpeeds(-400, -400);
+    panicTime = millis()+250;
+  }
+  Serial.print(sensorArr[0]);Serial.print("  ");Serial.println(sensorArr[1]);Serial.flush();
+}
